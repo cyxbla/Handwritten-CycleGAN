ccb7dae9b1811792051cc8c943552173455228de
cyxbla <snake96274@gapp.nthu.edu.tw>
2022-12-30 22:09:10 +0800

    add reload

--- demo.py
+++ demo.py
@@ -1,14 +1,18 @@
-from PIL import Image, ImageDraw, ImageFont, ImageTk
-import tkinter as tk
-from random import seed
-from random import gauss
 import random
 import subprocess
 import os
 import math
 import pywebio
-seed(44)
-# PATH = "C:\\Users\Hairy\Desktop\ML"
+from pywebio.input import * 
+from pywebio.output import * 
+from pywebio.session import run_js
+from PIL import Image, ImageDraw, ImageFont, ImageTk
+# from pywebio import run_js
+from random import seed
+from random import gauss
+
+seed(random.randint(0,99))
+
 base = "/mnt/c/home/hchiang/test/Handwritten-CycleGAN"
 input_text_path = 'buffer/input.txt'
 fonts = "buffer/kaiu.ttf"
@@ -16,9 +20,11 @@ text_img_pathsaveA = "datasets/predict/test/A/"
 text_img_pathsaveB = "datasets/predict/test/B/"
 hwr_img_pathsave = "output/A"
 log_buffer = "buffer/log.log"
+A2Bpath = "output/199_netG_B2A.pth"
 
 article_len = 0
 punctuation = {}
+newline = 0
 def init():
     global article_len
     subprocess.run(["rm", "-rf", f"{text_img_pathsaveA}"])
@@ -36,7 +42,7 @@ def show_result():
 def create_img():
     global article_len
     images = []
-    for i in range(article_len-1):
+    for i in range(article_len):
         tmp = Image.open("output/A/{:04d}.png".format(i))
         images.append(tmp.resize((64, 64)))
     max_width = 832
@@ -45,30 +51,33 @@ def create_img():
     
     col = max_width // img_width
     col = 15        # FIX
-    row = math.ceil(article_len / float(col))
+    row = math.ceil(article_len / float(col))+newline
     result = Image.new("RGB", (max_width, row*64), (255,255,255))
     x = 0
     y = 0       
     i = 0
     for image in images:
-        (l, t, r, b) = (3+random.randint(1,10), 0, 61-random.randint(1,10), 64)
-        if i not in punctuation:
-            (l, t, r, b) = (3+random.randint(1,10), 0, 61-random.randint(1,10), 64)
-        elif punctuation[i] == ['\n']:
-            print("new line")
-            y += img_height
-            x = 0
-        else:
-            (l, t, r, b) = (4, 4, 4, 4)            
-        box = (l, t, r, b)
+        if i in punctuation:
+            if punctuation[i] == 2:
+                y += img_height
+                x = 0
+                i += 1
+                continue
+            # else:
+            #     box = (10, 10, 50, 50) # (l, r, r, b)
+            #     image = image.crop(box)
+            #     im = Image.new("RGB", (random.randint(20, 30), 64), (255,255,255))
+            #     im.paste(image, (0,0))
+            #     if x + im.width > max_width:
+            #         x = 0
+            #         y += img_height
+        box = (3+random.randint(1,10), 0, 61-random.randint(1,10), 64) # (l, t, r, b)
         image = image.crop(box)
         im = Image.new("RGB", (random.randint(50, 60), 64), (255,255,255))
         im.paste(image, (random.randint(0,5), random.randint(0,5)))
-        # im.save("{:04d}.png".format(i))
         if x + im.width > max_width:
             x = 0
             y += img_height
-        
         result.paste(im, (x, y))
         i += 1
         x += im.width   
@@ -79,11 +88,11 @@ def create_img():
 
 def generate_handwritten():
     subprocess.run(["cp", "-rf", f"{text_img_pathsaveA}", f"{text_img_pathsaveB}"])
-    subprocess.run(["python3", "predict.py", "--cuda", "--dataroot", "datasets/predict", "--generator_A2B", "output/199_netG_B2A.pth"])
+    subprocess.run(["python3", "predict.py", "--cuda", "--dataroot", "datasets/predict", "--generator_A2B", f"{A2Bpath}"])
     create_img()
 
 def generate_text_image():
-    global article_len
+    global article_len, newline
     pix = 128
     s = 100
     shift = 14
@@ -91,6 +100,11 @@ def generate_text_image():
     with open(input_text_path, "r") as f:
         user_input = f.read()
     for i, ch in enumerate(user_input):
+        if(ch == '，' or ch == '。' or ch == '：' or ch == '？' or ch == '「' or ch == '」'):
+            punctuation[i] = 1
+        elif ch == '\n':
+            newline += 1
+            punctuation[i] = 2
         article_len += 1
         image = Image.new("RGB", (pix, pix), color=(255, 255, 255))
         draw = ImageDraw.Draw(image)
@@ -103,19 +117,12 @@ def generate_text_image():
     generate_handwritten()
 
 def getWord():
-    # init()
     arcle = pywebio.input.textarea('想要轉換的文章', rows=6)
-    # pywebio.input.actions('actions', [
-    #     {'label': 'Run', 'value': 'save'},
-    #     {'label': 'Reset', 'type': 'reset', 'color': 'warning'}
-    # ], help_text='actions')
-    # runCode()
     pywebio.output.put_text(arcle)
     with open(input_text_path, 'w') as f:
         f.write(arcle)
     generate_text_image()
-    pywebio.output.put_button("重新製作", onclick=lambda: init(), color='success', outline=True)
-    
+    pywebio.output.put_button("重新製作", onclick=lambda: run_js('window.location.reload()'), color='success', outline=True)
 
 if __name__ == '__main__':
-    pywebio.platform.tornado.start_server(init, port=8080, log_file="buffer/file.log", auto_reload=True)
+    pywebio.platform.tornado.start_server(init, port=8080, log_file="buffer/file.log", debug=True)
