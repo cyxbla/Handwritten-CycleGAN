827de7c513b5fdde4cd3771f6394c461d24b66a7
cyxbla <snake96274@gapp.nthu.edu.tw>
2023-01-04 22:33:42 +0800

    new version

--- demo.py
+++ demo.py
@@ -12,7 +12,6 @@ from PIL import Image, ImageDraw, ImageFont
 from random import seed
 seed(random.randint(0,99))
 
-base = "/mnt/c/home/hchiang/test/Handwritten-CycleGAN"
 input_text_path = 'buffer/input.txt'
 fonts = "buffer/kaiu.ttf"
 text_img_pathsaveA = "datasets/predict/test/A/"
@@ -20,11 +19,14 @@ text_img_pathsaveB = "datasets/predict/test/B/"
 hwr_img_pathsave = "output/A"
 log_buffer = "buffer/log.log"
 class Paths(Enum):
-    YenXuan = Path("output/yenxuan_199_netG_A2B.pth")
+    YenXuan = Path("output/path/yenxuan/yenxuan_199_netG_A2B.pth")
 
-    YuXuan = "output/yuxuan_299_netG_A2B.pth"
+    NienEn = "output/path/chris/chris_299_netG_A2B.pth"
 
-    XiZhi = "output/xizhi_199_netG_A2B.pth"
+    YuWen = "output/path/winwu/winwu_199_netG_A2B.pth"
+
+    YuXuan = "output/path/yuxuan/yuxuan_no_flip_159_netG_A2B.pth"
+
 
 article_len = 0
 punctuation = {}
@@ -49,16 +51,14 @@ def show_result():
     
     pywebio.output.put_grid([
         [pywebio.output.put_button("Download", onclick=lambda: pywebio.session.download('result.png', img), color='success', outline=True), 
-        pywebio.output.put_button("Reproduce", onclick=lambda: run_js('window.location.reload()'), color='success', outline=True)],
+        pywebio.output.put_button("Try Again", onclick=lambda: run_js('window.location.reload()'), color='success', outline=True)],
     ], cell_width='125px', cell_height='100px')
 
 def create_img():
+    global article_len
     pywebio.output.put_text("create image...")
     pywebio.output.put_processbar('create_img')
-    for i in range(1, 11):
-        pywebio.output.set_processbar('create_img', i / 10)
-        time.sleep(0.1)
-    global article_len
+    
     images = []
     for i in range(article_len):
         tmp = Image.open("output/A/{:04d}.png".format(i))
@@ -70,10 +70,13 @@ def create_img():
     col = max_width // img_width
     col = 15        # FIX
     row = math.ceil(article_len / float(col))+newline
-    result = Image.new("RGB", (max_width, row*64), (255,255,255))
+    
+    result = Image.open(f"background/{background}.png")
+    result = result.resize((max_width, row*64))
     x = 0
     y = 0       
     i = 0
+    
     for image in images:
         if i in punctuation:
             if punctuation[i] == 2:
@@ -81,14 +84,6 @@ def create_img():
                 x = 0
                 i += 1
                 continue
-            # else:
-            #     box = (10, 10, 50, 50) # (l, r, r, b)
-            #     image = image.crop(box)
-            #     im = Image.new("RGB", (random.randint(20, 30), 64), (255,255,255))
-            #     im.paste(image, (0,0))
-            #     if x + im.width > max_width:
-            #         x = 0
-            #         y += img_height
         box = (3+random.randint(1,10), 0, 61-random.randint(1,10), 64) # (l, t, r, b)
         image = image.crop(box)
         im = Image.new("RGB", (random.randint(50, 60), 64), (255,255,255))
@@ -96,9 +91,12 @@ def create_img():
         if x + im.width > max_width:
             x = 0
             y += img_height
-        result.paste(im, (x, y))
+        mask = get_mask(im)
+        result.paste(im, (x, y), mask)
         i += 1
-        x += im.width   
+        x += im.width 
+        pywebio.output.set_processbar('create_img', i / article_len)
+        time.sleep(0.1)  
         
     result.save("result.png")
     show_result()
@@ -106,20 +104,16 @@ def create_img():
 
 def generate_handwritten():
     pywebio.output.put_text("generate handwritten...")
-    subprocess.run(["cp", "-rf", f"{text_img_pathsaveA}", f"{text_img_pathsaveB}"])
-    subprocess.run(["python3", "predict.py", "--cuda", "--dataroot", "datasets/predict", "--generator_A2B", f"{Paths[selected].value}"])
     pywebio.output.put_processbar('generate_handwritten')
-    for i in range(1, 11):
-        pywebio.output.set_processbar('generate_handwritten', i / 10)
-        time.sleep(0.1)
+    subprocess.run(["cp", "-rf", f"{text_img_pathsaveA}", f"{text_img_pathsaveB}"])
+    pywebio.output.set_processbar('generate_handwritten', 5 / 10)
+    time.sleep(0.1)
+    subprocess.run(["python3", "predict.py", "--cuda", "--dataroot", "datasets/predict", "--generator_A2B", f"{Paths[selected].value}"])
+    pywebio.output.set_processbar('generate_handwritten', 1)
+    time.sleep(0.1)
     create_img()
 
 def generate_text_image():
-    pywebio.output.put_text("generate text image...")
-    pywebio.output.put_processbar('generate_text_image')
-    for i in range(1, 11):
-        pywebio.output.set_processbar('generate_text_image', i / 10)
-        time.sleep(0.1)
     global article_len, newline
     pix = 128
     s = 100
@@ -145,16 +139,31 @@ def generate_text_image():
     generate_handwritten()
 
 def getWord():
-    global selected
+    global selected, background
     arcle = pywebio.input.textarea('Auto Memory Dolls', rows=6, maxlength=500, minlength=1)
     pywebio.output.put_text(arcle)
-    selected = pywebio.input.select("Select Fonts:", ["YenXuan", "YuXuan", "XiZhi"])
+    selected = pywebio.input.select("Select Fonts:", ["YenXuan", "NienEn", "YuWen", "YuXuan"])
+    background = pywebio.input.select("Select Background:", ["defult","rainbow", "marble", "wood"])
     with open(input_text_path, 'w') as f:
         f.write(arcle)   
     
-    print(selected)
+    print(selected, background)
     generate_text_image()
-    # pywebio.output.put_button("Reproduce", onclick=lambda: run_js('window.location.reload()'), color='success', outline=True)
+
+def get_mask(pic):
+    width,height = pic.size
+    pixels = pic.load()
+    mask = Image.new("L",(width,height),255)
+    mask_p = mask.load()
+    for x in range(width):
+        for y in range(height):
+            r, g, b = pixels[x, y]
+            gray = (r+g+b) // 3
+            if gray > 122:
+                mask_p[x,y] = 0
+            else:
+                mask_p[x,y] = 255
+    return mask
 if __name__ == '__main__':
     pywebio.config(title="MLFianl Team 10 Demo")
-    pywebio.platform.tornado.start_server(init, port=8080, log_file="buffer/file.log", debug=True)
+    pywebio.platform.tornado.start_server(init, port=8080, log_file="buffer/file.log", debug=True, host="localhost")
